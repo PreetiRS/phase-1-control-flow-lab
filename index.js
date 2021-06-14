fc8900e3fae8cfb6d633b06525dc21d85a645b61
+ <+>
2021-06-13 18:25:22 -0700

    commit tree fa21b72ad2fb2bcdcc2fe121720d8b94d81da91a

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet) {
   // Write your code here!
+  if (feet < 400) {
+    return "This one is on me!";
+  }
+  else if (feet > 2500) {
+    return 'No can do.';
+  }
+  else if (feet > 2000) {
+    return 'I will gladly take your thirty bucks.';
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city) {
   // Write your code here!
+  const message = city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+  return message
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip) {
   // Write your code here!
+  let response;
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    //  break;
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+    }
+ 
 }
+//two options for return - the reason to use 'break' exits the switch case
+//
